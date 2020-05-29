4c320eaf8eeeca8aa8d1b6996e5f0007df560af4
70dbwjd81 <66100158+70dbwjd81@users.noreply.github.com>
2020-05-29 23:52:18 +0900

    Create Headset.java

---
+++ DesignPatternExamples/src/com/example/designpattern/composite/Headset.java
@@ -0,0 +1,30 @@
+package com.example.designpattern.composite;
+
+
+public class Headset extends ComputerDevice {
+
+	@Override
+
+	public int getPrice() {
+
+		// TODO Auto-generated method stub
+
+		return 0;
+
+	}
+
+
+
+	@Override
+
+	public int getPower() {
+
+		// TODO Auto-generated method stub
+
+		return 0;
+
+	}
+
+
+
+}
