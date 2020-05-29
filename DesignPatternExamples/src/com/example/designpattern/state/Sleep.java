300c08388b2f020bf2c17699cbe9ad8dfaaf6265
70dbwjd81 <66100158+70dbwjd81@users.noreply.github.com>
2020-05-30 00:20:56 +0900

    Create Sleep.java

---
+++ DesignPatternExamples/src/com/example/designpattern/state/Sleep.java
@@ -0,0 +1,19 @@
+package com.example.designpattern.state;
+
+public class Sleep implements State {
+
+	@Override
+
+	public void on_button(Light light) {
+
+		light.setState( new ON() );
+
+	}
+
+	@Override
+	public void off_button(Light light) {
+  
+  light.setState( new OFF());
+	}
+
+}
